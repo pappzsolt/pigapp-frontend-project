671953717ec53bea9559a21054d4a550af083937
pappzsol <papp.zsolt.gabor@gmail.com>
2025-03-30 22:44:18 +0200

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,7 +30,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit(){
     //this.authService.login().subscribe();
-    this.authService.login()
+    this.authService.login().subscribe()
     this.invoices$ = this.invoicesService.getInvoiceList();
   }
 
