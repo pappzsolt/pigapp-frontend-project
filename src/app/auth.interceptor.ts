24f50882793accf8880cdb664c1e482c11e4f697
pappzsol <papp.zsolt.gabor@gmail.com>
2025-03-31 21:54:30 +0200

    intercept

--- src/app/auth.interceptor.ts
+++ src/app/auth.interceptor.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpInterceptor, HttpEvent } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpRequest, HttpHandler, HttpInterceptor, HttpEvent, HttpErrorResponse } from '@angular/common/http';
+import { catchError, Observable, switchMap, throwError } from 'rxjs';
 import { AuthService } from './services/auth.service';  // Az AuthService-t az implementációdtól függően kell implementálni
 
 @Injectable()
-export class AuthInterceptor implements HttpInterceptor {
+/* export class AuthInterceptor implements HttpInterceptor {
 
   constructor(private authService: AuthService) {}
-    
+
     intercept(req: HttpRequest<any>,
           next: HttpHandler): Observable<HttpEvent<any>> {
           const token = this.authService.getJwtToken();
-          // const refresh = this.authService.getJwtRefresh();
-          //401 es hibat kezelni !!!!!!!!!!!!!!!!!!!hogy akkor ujra probalkozzon a token -el
+
           if (token) {
               const cloned = req.clone({
                   headers: req.headers.set("Authorization",
@@ -25,9 +24,32 @@ export class AuthInterceptor implements HttpInterceptor {
               return next.handle(req);
           }
         }
-}
+} */
+        @Injectable()
+        export class AuthInterceptor implements HttpInterceptor {
 
+          constructor(private authService: AuthService) {}
 
+          intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+            const token1 = this.authService.getJwtToken();
+            let authReq = req;
+            if (token1) {
+              authReq = this.addToken(req, token1);
+            }
+            return next.handle(authReq).pipe(
+              catchError((error: HttpErrorResponse) => {
+                const token2 = this.authService.getJwtToken();
+                if (error.status === 401 && token2) {
+                  return next.handle(this.addToken(req, token2));
+                }
+                return throwError(() => error);
+              })
+            );
+          }
 
-
-
+          private addToken(req: HttpRequest<any>, token: string) {
+            return req.clone({
+              headers: req.headers.set('Authorization', `Bearer ${token}`)
+            });
+          }
+        }
