0fb92356266d682837a108d4b159a21bcd65243b
pappzsol <papp.zsolt.gabor@gmail.com>
2025-04-11 21:16:57 +0200

    add menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,5 @@
 import { Component, Inject, inject, InjectionToken, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { InvoicesService } from './services/invoices.service';
-import { from, Observable, of } from 'rxjs';
-import { Invoice } from '../model/invoice';
-import { HttpClient } from '@angular/common/http';
-import { AuthService } from './services/auth.service';
-import { CashFlowServiceService } from './services/cash-flow.service.service';
-import { Cashflow } from '../model/cashflow';
-
 
 @Component({
   selector: 'app-root',
