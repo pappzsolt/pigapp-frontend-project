c2f0eb9f09521f59bc3c72e6c827db7fe3fee193
pappzsol <papp.zsolt.gabor@gmail.com>
2025-04-28 19:48:48 +0200

    costCurrentPage

--- src/app/monthly-calculation/monthly-calculation.component.ts
+++ src/app/monthly-calculation/monthly-calculation.component.ts
@@ -1,10 +1,6 @@
-import { Input, EventEmitter, Output } from '@angular/core';
 import { Invoice, InvoiceSummary, InvoiceWithCostDetail, TotalAmountInvoice } from '../../model/invoice';
-import { HighlightedDirective } from './../directives/highlighted.directive';
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { GroupByThreePipe } from '../pipe/group-by-three.pipe';
-import { GroupByPipe } from '../pipe/group-by.pipe';
 import { MonthlyCalculationService } from '../services/monthly-calculation.service';
 
 @Component({
@@ -21,11 +17,51 @@ export class MonthlyCalculationComponent implements OnInit {
   invoiceWithCosts: InvoiceWithCostDetail[] = [];
   isLoading = false;
   errorMessage = '';
+  currentPage = 1;
+  costCurrentPage: { [invoiceId: number]: number } = {};
+  pageSize = 5;
+  Math = Math;
 
   constructor(private monthlyCalculationService: MonthlyCalculationService) {}
   ngOnInit(): void {
     this.loadData();
   }
+
+  getPaginatedCosts(invoiceId: number, costs: any[]): any[] {
+    const currentPage = this.costCurrentPage[invoiceId] || 1;
+    const start = (currentPage - 1) * this.pageSize;
+    const end = start + this.pageSize;
+    return costs.slice(start, end);
+  }
+
+  nextCostPage(invoiceId: number, costs: any[]): void {
+    const totalPages = Math.ceil(costs.length / this.pageSize);
+    const currentPage = this.costCurrentPage[invoiceId] || 1;
+    if (currentPage < totalPages) {
+      this.costCurrentPage[invoiceId] = currentPage + 1;
+    }
+  }
+
+  prevCostPage(invoiceId: number): void {
+    const currentPage = this.costCurrentPage[invoiceId] || 1;
+    if (currentPage > 1) {
+      this.costCurrentPage[invoiceId] = currentPage - 1;
+    }
+  }
+
+  // A fő lapozás (számlák között)
+  prevPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+    }
+  }
+
+  nextPage() {
+    if (this.currentPage * this.pageSize < this.invoiceWithCosts.length) {
+      this.currentPage++;
+    }
+  }
+
   loadData(): void {
     this.isLoading = true;
     this.errorMessage = '';
@@ -48,12 +84,18 @@ export class MonthlyCalculationComponent implements OnInit {
 
     this.monthlyCalculationService.getInvoiceWithCostDetail().subscribe({
       next: data => {
-        this.invoiceWithCosts = data;
-        console.log(this.invoiceWithCosts); // Kiírja a tömböt a konzolra
+        if (Array.isArray(data)) {
+          this.invoiceWithCosts = data;
+        } else {
+          this.invoiceWithCosts = [data];
+        }
+        console.log(this.invoiceWithCosts);
       },
       error: err => this.handleError(err),
       complete: () => (this.isLoading = false),
     });
+
+
   }
   private handleError(error: any): void {
     console.error(error);
