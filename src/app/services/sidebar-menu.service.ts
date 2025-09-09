fa9190942f52278a9f0d4b9af6703061a24d2910
pappzsol <papp.zsolt.gabor@gmail.com>
2025-09-09 13:22:59 +0200

    sidevar

--- src/app/services/sidebar-menu.service.ts
+++ src/app/services/sidebar-menu.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CostSummary } from '../../model/invoice_sum_cost.model';
+import { ApiConfigService } from './api-config.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SidebarMenuService {
+  constructor(
+    private http: HttpClient,
+    private apiConfig: ApiConfigService
+  ) {}
 
-  constructor() { }
+  getMonthlySummary(): Observable<CostSummary[]> {
+    return this.http.get<CostSummary[]>(this.apiConfig.apiEnvironment.apiInfoUrl);
+  }
 }
--- src/app/sidebar-menu/sidebar-menu.component.ts
+++ src/app/sidebar-menu/sidebar-menu.component.ts
@@ -1,19 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarMenuService } from '../services/sidebar-menu.service';
 import { AuthService } from '../services/auth.service';
+import { CostSummary, CostGroupResponse, CostData } from '../../model/invoice_sum_cost.model';
 @Component({
   selector: 'app-sidebar-menu',
-  imports: [CommonModule,],
+  imports: [CommonModule],
   standalone: true,
   templateUrl: './sidebar-menu.component.html',
   styleUrl: './sidebar-menu.component.css',
 })
 export class SidebarMenuComponent {
+  summaries: CostSummary[] = [];
+  @Input()  totalPaid: number = 0;
+  @Input()  totalUnpaid: number = 0;
   constructor(
     private authService: AuthService,
     private sidebarMenuService: SidebarMenuService
   ) {}
+  calculateTotals(): void {
+    this.totalPaid = this.summaries.reduce((acc, curr) => acc + parseFloat(curr.total_paid), 0);
+    this.totalUnpaid = this.summaries.reduce((acc, curr) => acc + parseFloat(curr.total_unpaid), 0);
+  }
+  getMonthlySummary(): void {
+    this.sidebarMenuService.getMonthlySummary().subscribe({
+      next: data => {
+        this.summaries = data;
+        this.calculateTotals();
+      },
+    });
+  }
 
-ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getMonthlySummary();
+  }
 }
